5d218aa7d7aad6bf0068f161c5db66b8571c2328
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-02-06 23:19:40 -0500

    feat(menu-items): add destinations to menu items

---
+++ tourism-project/src/Components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Cusco',
+        path: '/cusco',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Arquipa',
+        path: '/arquipa',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Puno',
+        path: '/puno',
+        cName: 'dropdown-link'
+    },
+]
